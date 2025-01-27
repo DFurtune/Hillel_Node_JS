18d47c49b4fdb3b0615989f64ba6987ef95a828d
Dmytro Furtune <dmytro_furtune@epam.com>
2025-01-27 15:14:45 +0200

    add HW1 and HW2

---
+++ HW_2/index.js
@@ -0,0 +1,23 @@
+import Logger from "./logger/logger.js";
+
+const logger  = new Logger()
+
+logger.info("Info message")
+logger.warning("Warning message")
+logger.error("Error message")
+
+console.log({
+    APP_ENV: process.env['APP_ENV'],
+    DB_PASS: process.env['DB_PASS'],
+    PID: process.pid
+})
+
+const p = process.env
+
+
+setInterval(() => {
+    const timestemp = new Date().toISOString()
+
+    logger.info(`${timestemp}`)
+}, 10_000)
+
---
+++ HW_2/logger/formatter.js
@@ -0,0 +1,21 @@
+import chalk from "chalk";
+
+function formatMessage(level, msg) {
+  const timestamp = new Date().toISOString();
+
+  switch (level) {
+    case "info":
+      return chalk.blue(`[${timestamp}], INFO: ${msg}`);
+
+    case "warning":
+      return chalk.yellow(`[${timestamp}], WARNING: ${msg}`);
+
+    case "error":
+      return chalk.red(`[${timestamp}], ERROR: ${msg}`);
+
+    default:
+      return chalk.gray(`[${timestamp}], UNKNOW: ${msg}`);
+  }
+}
+
+export default formatMessage;
