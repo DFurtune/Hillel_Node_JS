3fb0704c9ebfa1a5cfb7563811de045e8c3d671f
Dmytro Furtune <dmytro_furtune@epam.com>
2025-04-13 10:07:40 +0300

    Add package.json and styles.css for HW13 project
    - Created package.json to manage project dependencies with Express and TypeScript types.
    - Added styles.css for basic styling of the application, including styles for body, headings, tables, and hover effects.

---
+++ Lesson7/HW13/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+const users = [
+  { name: "Олег", age: 25, email: "oleg@example.com" },
+  { name: "Марія", age: 30, email: "maria@example.com" },
+  { name: "Іван", age: 22, email: "ivan@example.com" },
+];
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  let tableRows = "";
+  users.forEach((user) => {
+    tableRows += `<tr><td>${user.name}</td><td>${user.age}</td><td>${user.email}</td></tr>`;
+  });
+
+  const html = `
+        <!DOCTYPE html>
+        <html lang="uk">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Таблиця користувачів</title>
+            <link rel="stylesheet" href="./public/style.css">
+        </head>
+        <body>
+            <h1>Список користувачів</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Ім'я</th>
+                        <th>Вік</th>
+                        <th>Електронна пошта</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${tableRows}
+                </tbody>
+            </table>
+        </body>
+        </html>
+    `;
+
+  res.send(html);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
