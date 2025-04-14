128c2a1e0fcaa8e0db61d8b4b0e14c87db7a8cbf
Dmytro Furtune <dmytro_furtune@epam.com>
2025-04-14 19:53:56 +0300

    feat: integrate Pug as the view engine and create user listing page

--- Lesson7/HW13/index.js
+++ Lesson7/HW13/index.js
@@ -8,42 +8,14 @@ const users = [
   { name: "Іван", age: 22, email: "ivan@example.com" },
 ];
 
+app.set("view engine", "pug");
+
+app.set("views", "./views");
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  let tableRows = "";
-  users.forEach((user) => {
-    tableRows += `<tr><td>${user.name}</td><td>${user.age}</td><td>${user.email}</td></tr>`;
-  });
-
-  const html = `
-        <!DOCTYPE html>
-        <html lang="uk">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Таблиця користувачів</title>
-            <link rel="stylesheet" href="./style.css">
-        </head>
-        <body>
-            <h1>Список користувачів</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Ім'я</th>
-                        <th>Вік</th>
-                        <th>Електронна пошта</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${tableRows}
-                </tbody>
-            </table>
-        </body>
-        </html>
-    `;
-
-  res.send(html);
+  res.render("index", { users });
 });
 
 app.listen(port, () => {
